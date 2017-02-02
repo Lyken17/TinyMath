d73250165ce71bdef4bd87ffb2e85d4f33097389
Lyken17 <lykensyu@gmail.com>
2017-02-01 21:42:14 -0800

    Clean unrelated files. Add c++ gitignore

---
+++ CMakeFiles/TinyMath.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TinyMath.dir/main.cpp.o"
+  "CMakeFiles/TinyMath.dir/TinyVector.cpp.o"
+  "TinyMath.pdb"
+  "TinyMath"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TinyMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
