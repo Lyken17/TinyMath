1b19c28a570070b7e0fbd2cf01e10ee033ad01b8
Lyken17 <lykensyu@gmail.com>
2017-02-03 17:12:19 -0800

    Implemented TinyColor class, not tested yet.

--- TinyColor.cpp
+++ TinyColor.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "TinyColor.h"
+
+typedef unsigned int colorInt;
+
+TinyColor::TinyColor(colorInt R, colorInt G, colorInt B, colorInt A) {
+    darray[0] = R;
+    darray[1] = G;
+    darray[2] = B;
+    darray[3] = A;
+}
+
+colorInt TinyColor::RGB2NUM() {
+    return RGB2NUM(this->darray[0], this->darray[1], this->darray[2], this->darray[3]);
+}
+
+colorInt TinyColor::RGB2NUM(std::tuple<colorInt, colorInt, colorInt> color) {
+    colorInt R, G, B;
+    std::tie(R, G, B) = color;
+    return RGB2NUM(R, G, B);
+}
+colorInt TinyColor::RGB2NUM(colorInt R, colorInt G, colorInt B, colorInt A) {
+    return (A << 24) + (R << 16) + (G << 8) + B;
+}
+
+std::tuple<colorInt, colorInt, colorInt> TinyColor::NUM2RGB(colorInt color) {
+    colorInt A, R, G, B;
+    A = (color & 0xff000000) >> 24;
+    R = (color & 0x00ff0000) >> 16;
+    G = (color & 0x0000ff00) >>  8;
+    B = (color & 0x000000ff) >>  0;
+    return std::make_tuple(R, G, B);
+}
--- TinyColor.h
+++ TinyColor.h
@@ -5,10 +5,28 @@
 #ifndef TINYMATH_TINYCOLOR_H
 #define TINYMATH_TINYCOLOR_H
 
+#include <tuple>
+
 #include "TinyVector"
 
-class TinyColor : TinyVector<int, 3>{
+typedef unsigned int colorInt;
 
+class TinyColor : TinyVector<colorInt, 4> {
+    // TODO: add common colors
+
+    // initialize by 3/4 nums
+    TinyColor(colorInt R, colorInt G, colorInt B, colorInt A = 0xff);
+
+    colorInt RGB2NUM();
+    static colorInt RGB2NUM(std::tuple<colorInt, colorInt, colorInt> color);
+    static colorInt RGB2NUM(colorInt R, colorInt G, colorInt B, colorInt A = 0xff);
+
+    static std::tuple<colorInt, colorInt, colorInt> NUM2RGB(colorInt color);
+
+
+    static const colorInt   RED   = 0xffff0000,
+                            GREEN = 0xff00ff00,
+                            BLUE  = 0xff0000ff;
 };
 
 
